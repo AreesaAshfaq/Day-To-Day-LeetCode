10bf41861094c665b0db76462601a2d76181db20
Areesa Ashfaq <85050863+Areesa110@users.noreply.github.com>
2024-08-01 11:41:48 +0500

    Time: 26 ms (98.67%) | Memory: 16.8 MB (6.97%) - LeetSync

---
+++ 677-map-sum-pairs/map-sum-pairs.py
@@ -0,0 +1,22 @@
+class MapSum:
+
+    def __init__(self):
+        self.hashMap = {}
+
+    def insert(self, key: str, val: int) -> None:
+        self.hashMap[key] = val 
+
+    def sum(self, prefix: str) -> int:
+        sum = 0
+        for k in self.hashMap:
+            print(k[:len(prefix)])
+            if prefix == k[:len(prefix)]:
+                sum += self.hashMap[k]
+        return sum
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
