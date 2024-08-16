3030591255c32368361db892050ed232adda8cde
Areesa Ashfaq <85050863+Areesa110@users.noreply.github.com>
2024-08-16 14:51:55 +0500

    Time: 104 ms (87.67%) | Memory: 17.8 MB (49.55%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,17 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        i = 0
-        while i <= len(nums):
-            if i not in nums:
-                return i
-            i += 1
+        #Approach 1: O(n)
+        n = len(nums)
+        total = (n*(n + 1))/2
+        for num in nums:
+            total -= num
+        return int(total)
+
+        #Approach 2: O(n2)
+        # i = 0
+        # while i <= len(nums):
+        #     if i not in nums:
+        #         return i
+        #     i += 1
+
+
