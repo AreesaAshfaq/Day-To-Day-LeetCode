3b4c2ee709c46f0555209f8e066ab6bdf3533da4
Areesa Ashfaq <85050863+AreesaAshfaq@users.noreply.github.com>
2025-07-20 17:54:18 +0500

    Time: 812 ms (46.25%) | Memory: 13 MB (77.55%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,37 +1,14 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        #Brute Force Appraoch : O(n2)
-       
-        # for i in range(len(nums)): #O(n)
-        #     for j in range(i+1, len(nums)): #O(n)
-        #         if nums[i] + nums[j] == target: #O(1)
-        #             return i, j
-        
-        # Using Two Pointers: O(nlogn)
-
-        numsS = sorted(nums)
-        l = 0
-        r = len(numsS)-1
-
-        while l < r:
-            if numsS[l] + numsS[r] > target:
-                r -= 1
-            elif numsS[l] + numsS[r] < target:
-                l += 1
-            else:
-                break
-
-        ans = []
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        Sum = sum(nums)
         for i in range(len(nums)):
-            if nums[i] == numsS[l]:
-                ans.append(i)
-            elif nums[i] == numsS[r]:
-                ans.append(i)
-
-        return ans
+            rem = target - nums[i]
+            if (target == rem + nums[i]) and rem in nums and nums.index(rem) != i:
+                return [nums.index(rem), i]
         
-            
-
-                
         
